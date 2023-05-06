ec41aed4c7e42b0453d8e30e0315abcf1d1bbd3f
Alejandro Ortega <ort18248@uvg.edu.gt>
2023-05-06 17:38:47 -0600

    Image slider

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,16 @@
-
+import styles from "../styles/page.module.css";
+import HomeSlideShow from "@/components/HomeSlideShow";
 import NavBar from "../components/NavBar";
 
 export default function Home() {
   return (
-    <NavBar></NavBar>
+    <div className={styles.main}>
+      <div className={styles.navBar}>
+        <NavBar></NavBar>
+      </div>
+      <div className={styles.slideShow}>
+        <HomeSlideShow></HomeSlideShow>
+      </div>
+    </div>
   )
 }
---
+++ src/components/HomeSlideShow.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../styles/HomeSlideShow.module.css"
+
+export default function() {
+    return (
+        <div className={styles.container}>
+
+        </div>
+    );
+}
